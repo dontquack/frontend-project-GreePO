bbcd4349e0f3ecc9aaf6d7729945eb0f912a71b6
Kseniya <akspost@yandex.ru>
2020-09-06 16:06:10 +0300

    The floating button 'Submit a request' is activated and modal windows are added.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function openForm(popup) {
+    document.getElementById(popup).style.display = 'block'
+    document.getElementById('blackout').style.display = 'block'
+}
+
+function closeForm(popup) {
+    document.getElementById(popup).style.display = 'none'
+    document.getElementById('blackout').style.display = 'none'
+}
